2d7bc043b215c1d1fad27a4e0bccf0b4f75e0511
Tuguldur66 <tuguutuguldur335@yahoo.com>
2025-12-25 09:47:22 +0800

    list

---
+++ script.js
@@ -0,0 +1,51 @@
+const input = document.querySelector("input");
+const submitBtn = document.querySelector("button");
+const list = document.querySelector(".list");
+const clearBtn = document.querySelector(".clear");
+
+let editElement = null;
+let isEditing = false;
+submitBtn.addEventListener("click", () => {
+  const value = input.value;
+
+  if (value === "") return;
+
+  if (isEditing) {
+    editElement.textContent = value;
+    submitBtn.textContent = "Submit";
+    isEditing = false;
+    editElement = null;
+  } else {
+    addItem(value);
+  }
+
+  input.value = "";
+});
+function addItem(text) {
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    <p>${text}</p>
+    <div class="actions">
+      <button class= "edit">edit</button>
+      <button class="delete">delete</button>
+    </div>
+  `;
+
+  list.appendChild(li);
+
+  li.querySelector(".delete").addEventListener("click", () => {
+    li.remove();
+  });
+
+  li.querySelector(".edit").addEventListener("click", () => {
+    input.value = li.querySelector("span").textContent;
+    submitBtn.textContent = "Edit";
+    isEditing = true;
+    editElement = li.querySelector("span");
+  });
+}
+
+clearBtn.addEventListener("click", () => {
+  list.innerHTML = "";
+});
